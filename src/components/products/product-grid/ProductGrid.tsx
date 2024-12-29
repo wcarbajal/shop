5c8c34706f908a06650c28e79ac3e0f40cf89bac
wuilmer carbajal <wcarbajal@outlook.com>
2024-12-29 14:13:11 -0500

    inicio de tienda

---
+++ src/components/products/product-grid/ProductGrid.tsx
@@ -0,0 +1,23 @@
+import { Product } from '@/interfaces';
+import { ProductGridItem } from './ProductGridItem';
+
+interface Props {
+  products: Product[];
+}
+
+
+export const ProductGrid = ( { products }: Props ) => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 mb-10">
+      {
+        products.map( product => (
+          <ProductGridItem
+            key={ product.slug }
+            product={ product }
+          />
+        ) )
+      }
+
+    </div>
+  );
+};
---
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { Product } from '@/interfaces';
+import { useState } from 'react';
+
+interface Props {
+  product: Product;
+}
+
+
+export const ProductGridItem = ( { product }: Props ) => {
+
+  const [ displayImage, setDisplayImage ] = useState( product.images[ 0 ] );
+
+
+  return (
+    <div className="rounded-md overflow-hidden fade-in ">
+      <Link href={ `/product/${ product.slug }` }>
+        <Image
+          src={ `/products/${ displayImage }` }
+          alt={ product.title }
+          className="w-full h-2/3 object-cover rounded"
+          width={  500 }
+          height={ 500 }
+          onMouseEnter={ () => setDisplayImage( product.images[1] )  }
+          onMouseLeave={ () => setDisplayImage( product.images[0] ) }
+        />
+      </Link>
+
+      <div className="p-4 flex flex-col gap-y-2">
+        <Link
+          className="hover:text-blue-600 font-bold"
+          href={ `/product/${ product.slug }` }>
+          { product.title }
+        </Link>
+        <span>{product.description}</span>
+        <span className="font-bold">${ product.price }</span>
+      </div>
+
+    </div>
+  );
+};
